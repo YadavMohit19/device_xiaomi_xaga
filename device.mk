c7c51ad1e47ecd52aa596a38f5fe5104e728a33f
YadavMohit19 <mohityadav050575@gmail.com>
2022-10-21 18:01:46 +0530

    xaga: drop libshim_vt service * Don't need in S vendor * fix volte issue after rebooting Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- device.mk
+++ device.mk
@@ -198,7 +198,6 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_PACKAGES += \
     ImsServiceBase \
-    libshim_vtservice
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
